9e3d1793dc360ec0d794ea768b22b1c1db2b4360
VDJoseluis <vdjoseluis@outlook.com>
2024-11-07 19:18:49 +0100

    Correccion de origin de vercel en la API

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ["https://freelance-help-app.vercel.app/", "http://localhost:5173"];
+const allowedOrigins = ["https://freelance-help-app.vercel.app/"];
 app.use(cors({
   origin: allowedOrigins,
   methods: "POST",
