ffe49865a5f025867680bacd48fd3647aad8f616
VDJoseluis <vdjoseluis@outlook.com>
2024-11-07 20:06:54 +0100

    Responder exitosamente a todas las solicitudes preflight

--- server/index.js
+++ server/index.js
@@ -1,17 +1,36 @@
 import express from "express";
+import cors from "cors";
 import generateInvoice from "./invoice.js";
 import helpFillingForm130 from "./helpFillingForm130.js";
-import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://freelance-help-app.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
+
 app.use(express.json());
 
 app.post("/generate-invoice", generateInvoice);
 app.post("/help-filling-form130", helpFillingForm130);
-
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en puerto:${port}`);
