6a14fdcb6521f6c7da17f77dfbc3a0c71491adb6
VDJoseluis <vdjoseluis@outlook.com>
2024-11-07 19:16:04 +0100

    Añadir origin de vercel en la API

--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({origin: "http://localhost:5173"}));
+const allowedOrigins = ["https://freelance-help-app.vercel.app/", "http://localhost:5173"];
+app.use(cors({
+  origin: allowedOrigins,
+  methods: "POST",
+  credentials: true
+}));
 
 app.use(express.json());
 
